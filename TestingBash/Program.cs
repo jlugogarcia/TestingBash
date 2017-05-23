c3ee82583a066b199d7a9bd6e76f0e05043eca25
jlugogar - Jose Lugo <jalugogarcia@gmail.com>
2017-05-23 15:46:48 -0400

    Add project files.

---
+++ TestingBash/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestingBash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunJob();
+        }
+
+        private static void RunJob()
+        {
+            string logFolder = "C:\\MyLogs";
+
+            if ((!System.IO.Directory.Exists(logFolder)))
+            {
+                System.IO.Directory.CreateDirectory(logFolder);
+            }
+
+            string pathFile = Path.GetFullPath(logFolder) + "\\" + DateTime.Now.ToString("MM_dd_yyyy_HH_mm") + "_Log.txt";
+
+
+            try
+            {
+                if ((!File.Exists(pathFile)))
+                {
+                    File.Create(pathFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                string logPath = "C:\\TestLog.txt";
+                File.AppendAllText(logPath, Environment.NewLine + ex.Message);
+            }
+        }
+    }
+}
